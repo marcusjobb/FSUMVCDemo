e87dd72e5fe03b0c470f6cf39098db75d7377b11
Marcus Medin <medina.jobb@outlook.com>
2022-08-29 13:39:32 +0200

    Add project files.

---
+++ FSUMVCDemo/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿namespace FSUMVCDemo.Controllers
+{
+    using System.Diagnostics;
+    using FSUMVCDemo.Models;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        // Get('/')
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // Get('/privacy')
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Name(string name)
+        {
+            ViewData["Hello"] = "Hej " + name;
+            return View("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
